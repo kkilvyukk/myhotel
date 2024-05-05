cca2ee9129275eacde9f6f7412b7318ff173cd9c
User <kurtinivan7@gmail.com>
2024-05-06 00:31:30 +0300

    add datePicker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,5 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.savvi.datepicker:rangepicker:1.3.0")
+
 }
--- app/src/main/java/com/example/retrofitmy/bottomnav/reservation/ReservationFragment.java
+++ app/src/main/java/com/example/retrofitmy/bottomnav/reservation/ReservationFragment.java
@@ -8,48 +8,55 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
 import com.example.retrofitmy.R;
 import com.example.retrofitmy.databinding.FragmentReservationBinding;
 import com.example.retrofitmy.databinding.FragmentSearchBinding;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 public class ReservationFragment extends Fragment {
 
     private FragmentReservationBinding binding;
 
+    TextView dateRangeText;
+    MaterialButton calendar;
 
-    CalendarView calendarView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentReservationBinding.inflate(inflater, container, false);
 
-        CalendarView calendarView = binding.getRoot().findViewById(R.id.calendarView);
+        TextView dateRangeText = binding.getRoot().findViewById(R.id.text);
+        MaterialButton calendar = binding.getRoot().findViewById(R.id.calendar);
+        MaterialDatePicker materialDatePicker = MaterialDatePicker.Builder.dateRangePicker().
+                setSelection(Pair.create(MaterialDatePicker.thisMonthInUtcMilliseconds(), MaterialDatePicker.todayInUtcMilliseconds())).build();
 
-
-
-
-
-
-        calendarView.setOnDateChangeListener((view, year, month, dayOfMonth) -> {
-            int mYear = year;
-            int mMonth = month;
-            int mDay = dayOfMonth;
-            String selectedDate = new StringBuilder().append(mMonth + 1)
-                    .append("-").append(mDay).append("-").append(mYear)
-                    .append(" ").toString();
-            Toast.makeText(getContext(), selectedDate, Toast.LENGTH_LONG).show();
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                materialDatePicker.show(getActivity().getSupportFragmentManager(), "Tag_picker");
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+                        dateRangeText.setText(materialDatePicker.getHeaderText());
+                    }
+                });
+            }
         });
+
+
         return binding.getRoot();
     }
 
-    private Context getApplicationContext() {
-        return null;
-    }
 }
